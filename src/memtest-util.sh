d82e8f3aa8093632c74fd82f7bba4fd302d78c45
Christian Wagner <christianwagner@system76.comn>
2022-06-03 11:59:47 -0500

    Added `options` parameter to `memtest-util.sh` script for setting custom boot options

--- src/memtest-util.sh
+++ src/memtest-util.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-# This script is for copying the `memtest` files to the ESP, deleting them, or adjusting their launch parameters. It is intended to be accessible to both the package system and to end-users.
+# This script is for copying the `memtest` files to the ESP, deleting them, or adjusting their launch parameters. It is intended to be accessible to 
+# both the package system and to end-users.
 #
+# The script is run with a verb, such as:
+# ```
+# memtest-util reinstall
+# ```
 # This script is, per deb package guidelines, written to be POSIX-compliant and avoid anything `bash`-specific. 
 #
 # The EFI executeable is copied to `/boot/efi/EFI/memtest/memtest.efi`, and the basic form of the `/boot/efi/loader/entries/memtest.conf` file is:
-#
+# ```
 # title memtest
 # efi /EFI/memtest/memtest.efi
-#
-# There is also an optional "options" line which is documented at https://github.com/memtest86plus/memtest86plus/blob/main/README.md , and this script will allow changes to that line in a future revision.
+# ```
+# There is also an optional "options" line which is documented at https://github.com/memtest86plus/memtest86plus/blob/main/README.md ,
+# and which this script can use using the "options" verb, followed by the requested options as a quoted, space-delineated string. For example:
+# ```
+# memtest-util options "nosmp keyboard=legacy"
+# ```
+# will add the line
+# ```
+# options nosmp keyboard=legacy
+# ```
+# to the `memtest.conf` file. Run `memtest-util reinstall` to remove any existing options.
 
 if [ "$(id -u)" -ne 0 ];
 then
@@ -19,6 +33,7 @@ fi
 
 argument1="$1"
 script_verb=${argument1:?"Comamnd verb missing, valid verbs are 'reinstall' and 'uninstall'."}
+argument2="$2"
 
 func_copy_EFI_to_ESP () {
     echo "copying memtest.efi to ESP"
@@ -29,10 +44,20 @@ func_copy_EFI_to_ESP () {
 func_write_conf_to_ESP () {
     echo "removing existing memtest.conf from ESP"
     rm -f /boot/efi/loader/entries/memtest.conf
-    echo "writing new memtest.conf to ESP"
+    echo "writing new default memtest.conf to ESP"
     echo "# written by memtest-util.sh" >> /boot/efi/loader/entries/memtest.conf
     echo "title memtest" >> /boot/efi/loader/entries/memtest.conf
     echo "efi /EFI/memtest/memtest.efi" >> /boot/efi/loader/entries/memtest.conf
+}
+
+func_write_conf_with_options_to_ESP () {
+    echo "removing existing memtest.conf from ESP"
+    rm -f /boot/efi/loader/entries/memtest.conf
+    echo "writing new memtest.conf with options $1 to ESP"
+    echo "# written by memtest-util.sh" >> /boot/efi/loader/entries/memtest.conf
+    echo "title memtest" >> /boot/efi/loader/entries/memtest.conf
+    echo "efi /EFI/memtest/memtest.efi" >> /boot/efi/loader/entries/memtest.conf
+    echo "options $1" >> /boot/efi/loader/entries/memtest.conf
 }
 
 func_remove_from_ESP () {
@@ -48,6 +73,9 @@ case "$script_verb" in
     ;;
     "uninstall") func_remove_from_ESP   
     ;;
-    *) echo "Unrecognized verb, valid verbs are 'reinstall' and 'uninstall'."
+    "options") options_string=${argument2:?"Options string missing, please use a quoted, space-delineated string."}
+    func_write_conf_with_options_to_ESP "$options_string"
+    ;;
+    *) echo "Unrecognized verb, valid verbs are 'reinstall', 'uninstall', and 'options'."
     ;;
 esac
